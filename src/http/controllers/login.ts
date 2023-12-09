b12a103bb21e6ac7a37e6135fe3949a2f6ad45e6
ronalt4cs <ronaltaugustocs@gmail.com>
2023-12-09 14:12:56 -0300

    refact: remove trycatch

--- src/http/controllers/login.ts
+++ src/http/controllers/login.ts
@@ -1,32 +1,15 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
-import { z } from 'zod'
-import { InvalidateCredentialsError } from '@/services/errors/invalidate-credentials-error'
 import { MakeAuthenticateService } from '@/services/factories/make-authenticate-service'
+import { loginBodySchema } from '../helpers/users-schemas'
 
-export async function login(
-  request: FastifyRequest,
-  reply: FastifyReply,
-) {
-  const registerBodySchema = z.object({
-    email: z.string().email(),
-    password: z.string().min(6),
+export async function login(request: FastifyRequest, reply: FastifyReply) {
+  const { email, password } = loginBodySchema.parse(request.body)
+
+  const authenticateService = MakeAuthenticateService()
+  await authenticateService.execute({
+    email,
+    password,
   })
 
-  const { email, password } = registerBodySchema.parse(request.body)
-
-  try {
-    const authenticateService = MakeAuthenticateService()
-    await authenticateService.execute({
-      email,
-      password,
-    })
-  } catch (error) {
-    if (error instanceof InvalidateCredentialsError) {
-      return reply.status(409).send({ message: error.message })
-    }
-
-    throw error
-  }
-
-  return reply.status(201).send()
+  return reply.status(200).send()
 }
